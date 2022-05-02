e3aa8ff5827c8c579bc1ebbc19c337af9a4b2ce3
Drew Halverson <drew@navlys.com>
2022-05-02 14:25:38 -0500

    modified app.js file

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Drew Halverson - Cloud for Developers - Project 4
         </p>
         <a
           className="App-link"
